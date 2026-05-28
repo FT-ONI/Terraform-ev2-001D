c2aeff48875e2472cfa6221191e6d50631581fba
PAAPCA02TR0514 <Duoc@PAAPCA02TR0514.duoc.acad>
2026-05-28 16:50:44 -0400

    se modifica archivo main.tf para evitar errores en las pruebas ademas de modifica archivo changelog

--- main.tf
+++ main.tf
@@ -13,23 +13,24 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/FT-ONI/VPC-ev2.git?ref=v1.0.0"
+  # Eliminamos ?ref=v1.0.0 para que descargue la rama por defecto
+  source = "git::https://github.com/FT-ONI/VPC-ev2.git" 
 }
 
 module "ec2" {
-  source            = "git::https://github.com/FT-ONI/computing-ev2.git?ref=v1.0.0"
+  source            = "git::https://github.com/FT-ONI/computing-ev2.git"
   vpc_id            = module.vpc.vpc_id
   subnet_id         = module.vpc.subnet_public_1_id
   security_group_id = module.vpc.sg_web_id
 }
 
 module "load_balancer" {
-  source           = "git::https://github.com/FT-ONI/LB-ev2.git?ref=v1.0.0"
+  source           = "git::https://github.com/FT-ONI/LB-ev2.git"
   sg_alb_id        = module.vpc.sg_alb_id
   public_subnets   = [module.vpc.subnet_public_1_id, module.vpc.subnet_public_2_id]
   target_group_arn = module.ec2.target_group_arn
 }
 
 module "s3" {
-  source = "git::https://github.com/FT-ONI/s3-ev2.git?ref=v1.0.0"
+  source = "git::https://github.com/FT-ONI/s3-ev2.git"
 }
